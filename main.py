b495697cf47a80781ad4ee38eebdf5c5e02af5c5
Pavel <ikpas21041997@mail.ru>
2022-10-16 00:31:10 +0500

    Add comments

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 def count_buyers(n_customers: int) -> dict:
+    '''
+    :param n_customers: Количество клиентов.
+    :return: Словарь, где ключ - номер группы, значение - количество человек в данной группе.
+    '''
     result = {}
     for id in range(n_customers):
         number_group = sum(map(int, str(id)))
@@ -9,6 +13,11 @@ def count_buyers(n_customers: int) -> dict:
 
 
 def count_buyers_in_range(first_id: int, n_customers: int) -> dict:
+    '''
+    :param first_id: ID первого клиента.
+    :param n_customers: Количество клиентов.
+    :return: Словарь, где ключ - номер группы, значение - количество человек в данной группе.
+    '''
     result = {}
     for id in range(first_id, first_id + n_customers):
         number_group = sum(map(int, str(id)))
